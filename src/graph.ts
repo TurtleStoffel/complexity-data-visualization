7e39f76c4b151fbaa95dbc48538f91da3988acdb
TurtleStoffel <me@turtlestoffel.com>
2024-12-08 18:28:45 +0100

    Render graph using D3

--- src/graph.ts
+++ src/graph.ts
@@ -1,6 +1,62 @@
-import * as Plot from "@observablehq/plot"
+import * as d3 from "d3"
+
+interface CustomNode extends d3.SimulationNodeDatum {
+    id: string;
+}
 
 export function setupPlot(element: HTMLDivElement) {
-    const plot = Plot.rectY({length: 10000}, Plot.binX({y: "count"}, {x: Math.random})).plot();
-    element.append(plot);
+    const width = 640;
+    const height = 400;
+
+    const nodes: CustomNode[] = [{
+        id: "test-1"
+    }, {
+        id: "test-2"
+    }]
+    const links: d3.SimulationLinkDatum<CustomNode>[] = [{
+        source: "test-1",
+        target: "test-2"
+    }]
+
+    const simulation = d3.forceSimulation(nodes)
+        .force("link", d3.forceLink(links).id(d=> (d as CustomNode).id))
+        .force("charge", d3.forceManyBody())
+        .force("x", d3.forceX())
+        .force("y", d3.forceY());
+
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [-width/2, -height/2, width, height]);
+
+    // Add a line for each link, and a circle for each node.
+    const link = svg.append("g")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.6)
+        .selectAll("line")
+        .data(links)
+        .join("line")
+        .attr("stroke-width", 2);
+
+    const node = svg.append("g")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 1.5)
+        .selectAll("circle")
+        .data(nodes)
+        .join("circle")
+        .attr("r", 5)
+
+    simulation.on("tick", () => {
+        link
+            .attr("x1", d => (d.source as CustomNode).x!!)
+            .attr("y1", d => (d.source as CustomNode).y!!)
+            .attr("x2", d => (d.target as CustomNode).x!!)
+            .attr("y2", d => (d.target as CustomNode).y!!);
+
+        node
+            .attr("cx", d => d.x!!)
+            .attr("cy", d => d.y!!);
+    })
+
+    element.append(svg.node()!!);
 }
