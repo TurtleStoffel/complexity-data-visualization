13aaf83f7e90d9e4c2265c173b08a49b320066ca
TurtleStoffel <me@turtlestoffel.com>
2024-12-16 08:46:41 +0100

    Strongly type Backend Metrics

--- src/plot.ts
+++ src/plot.ts
@@ -1,16 +1,22 @@
 import * as Plot from '@observablehq/plot'
 import * as d3 from 'd3'
 
+interface SizeMetric {
+    path: string;
+    size: number;
+}
+
 export async function setupPlot(element: HTMLDivElement) {
     console.log("Loading data from Complexity")
 
-    const metrics = await d3.json('./metrics.json') as [object]
+    const metrics = await d3.json('./metrics.json') as [SizeMetric]
+    metrics.sort((a, b) => d3.descending(a.size, b.size))
 
     console.log(metrics);
 
     const plot = Plot.plot({
         marks: [
-            Plot.rectY(metrics, Plot.binX({y: "count"}, {x: "size"})),
+            Plot.rectY(metrics, Plot.binX({y: "count"}, {x: "size", thresholds: 20})),
         ]
     })
 
